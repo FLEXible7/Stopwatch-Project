d5b1026d7ff3223270ab5b7ec1bdc09065327bc2
FLEXible7 <48522218+FLEXible7@users.noreply.github.com>
2019-03-14 19:29:21 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Stopwatch
+{
+    partial class Stopwatch
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Stopwatch));
+            this.launchButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.spanLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // launchButton
+            // 
+            this.launchButton.BackColor = System.Drawing.SystemColors.Window;
+            this.launchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.launchButton.Location = new System.Drawing.Point(12, 35);
+            this.launchButton.Name = "launchButton";
+            this.launchButton.Size = new System.Drawing.Size(166, 66);
+            this.launchButton.TabIndex = 0;
+            this.launchButton.Text = "Start";
+            this.launchButton.UseVisualStyleBackColor = false;
+            this.launchButton.Click += new System.EventHandler(this.launchButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.SystemColors.Window;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetButton.Location = new System.Drawing.Point(463, 35);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(166, 66);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // spanLabel
+            // 
+            this.spanLabel.AutoSize = true;
+            this.spanLabel.Font = new System.Drawing.Font("Arial Unicode MS", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.spanLabel.Location = new System.Drawing.Point(184, 37);
+            this.spanLabel.Name = "spanLabel";
+            this.spanLabel.Size = new System.Drawing.Size(252, 64);
+            this.spanLabel.TabIndex = 2;
+            this.spanLabel.Text = "00:00:000";
+            this.spanLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Stopwatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(645, 133);
+            this.Controls.Add(this.spanLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.launchButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Stopwatch";
+            this.Text = "STOPWATCH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button launchButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label spanLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Stopwatch : Form
+    {
+        TimeSpan span = TimeSpan.Zero; //Отсчитываемый интервал
+        DateTime startTime;
+        public Stopwatch()
+        {
+            InitializeComponent();
+            launchButton.ForeColor = Color.FromArgb(30, 144, 255);
+            resetButton.ForeColor = Color.FromArgb(30, 144, 255);
+        }
+
+        private void launchButton_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                span += DateTime.Now - startTime; //К интервалу добавляется время между последним запуском и настоящим моментом
+                resetButton.Enabled = true;
+                launchButton.BackColor = Color.White;
+                launchButton.ForeColor = Color.FromArgb(30, 144, 255);
+            }
+
+            else
+            {
+                startTime = DateTime.Now;
+                resetButton.Enabled = false;
+                launchButton.BackColor = Color.FromArgb(30, 144, 255);
+                launchButton.ForeColor = Color.White;
+            }
+            timer1.Enabled = !timer1.Enabled;
+            launchButton.Text = timer1.Enabled ? "Pause" : "Continue";
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            span = TimeSpan.Zero; //При нажатии кнопки сброса интервал обнуляется, т.к. отсчёт начинается заново
+            spanLabel.Text = "00:00:000";
+            launchButton.Text = "Start";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan newSpan = span + (DateTime.Now - startTime); //текущий интервал суммируется с временем ожидания
+            spanLabel.Text = newSpan.Minutes.ToString("00") + ":" + newSpan.Seconds.ToString("00") + ":" + newSpan.Milliseconds.ToString("000");
+        }
+    }
+}
